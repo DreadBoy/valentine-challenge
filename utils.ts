60d1f8bbacfc0d95f11d30644af8969ebf407b2a
Matic Leva <vertical3life@gmail.com>
2018-02-20 18:43:30 +0100

    Add places to dropdown

--- utils.ts
+++ utils.ts
@@ -17,6 +17,10 @@ class Point {
     bearingTo(point: Point): number {
         return Math.atan2(point.x - this.x, point.y - this.y) * (180 / Math.PI);
     }
+
+    distanceTo(point: Point): number {
+        return Math.sqrt(Math.pow(point.x - this.x, 2) + Math.pow(point.y - this.y, 2));
+    }
 }
 
 export {
